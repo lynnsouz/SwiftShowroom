4bce0c886f875e7d6697cb03e491433291c798bb
Lynneker Souza <lynsouza@icloud.com>
2019-10-14 19:36:58 -0300

    New project: ColorGuessGame

---
+++ ColorGuessGame/ColorGuessGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorGuessGame
+//
+//  Created by Lynneker Souza on 14/10/19.
+//  Copyright © 2019 Lynneker Souza. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
