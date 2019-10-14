1b8f06be55c1a6e980a490c7620bb808d929bc82
Lynneker Souza <lynsouza@icloud.com>
2019-10-14 20:12:19 -0300

    Create interface with SwiftUI for the game.

--- ColorGuessGame/ColorGuessGame/ContentView.swift
+++ ColorGuessGame/ColorGuessGame/ContentView.swift
@@ -9,13 +9,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    let rTarget = Double.random(in: 0..<1)
+    let gTarget = Double.random(in: 0..<1)
+    let bTarget = Double.random(in: 0..<1)
+    @State var rGuess = Double.random(in: 0..<1)
+    @State var gGuess = Double.random(in: 0..<1)
+    @State var bGuess = Double.random(in: 0..<1)
     var body: some View {
-        Text("Hello World")
+        VStack {
+            HStack {
+                // Target color block
+                VStack {
+                    Rectangle()
+                    .foregroundColor(Color(red: rTarget, green: gTarget, blue: bTarget, opacity: 1.0))
+                    Text("Match this color")
+                }
+                // Guess color block
+                VStack {
+                    Rectangle()
+                    .foregroundColor(Color(red: rGuess, green: gGuess, blue: bGuess, opacity: 1.0))
+                    HStack {
+                      Text("R: \(Int(rGuess * 255.0))")
+                      Text("G: \(Int(gGuess * 255.0))")
+                      Text("B: \(Int(bGuess * 255.0))")
+                    }
+                }
+            }
+            Text("Hit me button")
+            
+            VStack {
+                ColorSlider(value: $rGuess, textColor: .red)
+                ColorSlider(value: $gGuess, textColor: .green)
+                ColorSlider(value: $bGuess, textColor: .blue)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+       ContentView()
+    }
+}
+
+struct ColorSlider: View {
+    @Binding var value: Double
+    var textColor: Color
+    var body: some View {
+        HStack {
+            Text("0")
+              .foregroundColor(textColor)
+            Slider(value: $value)
+            Text("255")
+              .foregroundColor(textColor)
+        }
+        .padding()
     }
 }
